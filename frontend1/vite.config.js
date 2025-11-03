3e7b37b8b1373435aa5c411311b1345d74f8b357
Junaidk007 <junaid.k0004@gmail.com>
2025-11-03 23:24:26 +0530

    allow host

--- frontend1/vite.config.js
+++ frontend1/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    allowedHosts: [
+      'ai-notes-assistant-i47e.onrender.com' // 👈 Add your Render backend domain here
+    ],
+  },
 })
