50418a79626c218fdf1602eb86f9fe2b39550670
Junaidk007 <junaid.k0004@gmail.com>
2025-11-04 23:05:49 +0530

    added date and time in history page

--- frontend1/src/HistoryBox.jsx
+++ frontend1/src/HistoryBox.jsx
@@ -2,19 +2,32 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { MyContext } from "./MyContext";
 
-function HistoryBox({data, setData}) {
-    let {note, setNote, setReply, setTitle} = useContext(MyContext);
+function HistoryBox({ data, setData }) {
+    let { note, setNote, setReply, setTitle } = useContext(MyContext);
+
+    const date = new Date(data.timestamp);
+    const time = new Date(data.timestamp);
+
+    const formattedDate = date.toLocaleDateString("en-GB", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+    const formattedTime = time.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
 
     const deleteNotes = async () => {
         const id = data._id;
         try {
-            let response = await fetch(`https://ainotesassistant.onrender.com/api/notes/${id}`, {method: 'DELETE'});
+            let response = await fetch(`https://ainotesassistant.onrender.com/api/notes/${id}`, { method: 'DELETE' });
             let mes = await response.json();
             setData((prevNotes) => prevNotes.filter(note => note._id !== id));
         } catch (e) {
             console.log(e);
         }
-        
+
     }
 
     const getNotes = async () => {
@@ -37,11 +50,12 @@ function HistoryBox({data, setData}) {
         <div class="card mb-3">
             <Link onClick={getNotes} to={"/"}>
                 <div className="card-body justify-content-between d-flex align-items-center p-4">
-                    <div style={{width: "95%"}}>
-                        <h5 className="card-title border-bottom pb-3 mb-3" style={{color: "#353535",width: "100%"}}>{data.title}</h5>
-                        <p className="card-text" style={{color: "#353535"}}>{plainText}</p>
+                    <div style={{ width: "95%" }}>
+                        <h5 className="card-title " style={{ color: "#353535", width: "100%" }}>{data.title}</h5>
+                        <p style={{ color: "black", fontSize: "0.75rem" }} className="text-muted border-bottom pb-2 mb-3">{formattedDate}&nbsp;&nbsp;&#124;&nbsp;&nbsp;{formattedTime}</p>
+                        <p className="card-text" style={{ color: "#353535" }}>{plainText}</p>
                     </div>
-                       <i onClick={deleteNotes} className="fa-solid fa-trash" style={{color: "#ff6b01"}}></i>
+                    <i onClick={deleteNotes} className="fa-solid fa-trash" style={{ color: "#ff6b01" }}></i>
                 </div>
             </Link>
         </div>
--- frontend1/src/HistoryPage.jsx
+++ frontend1/src/HistoryPage.jsx
@@ -1,13 +1,25 @@
 import HistoryBox from "./HistoryBox";
-import { useState, useEffect } from "react";
+import { RiseLoader } from 'react-spinners'
+import { useState, useEffect, useContext } from "react";
+import { MyContext } from "./MyContext";
 
 function HistoryPage() {
-    let [ data, setData ] = useState([]);
+    let { setLoading, loading } = useContext(MyContext)
+    let [data, setData] = useState([]);
+
+    let override = {
+        display: "inline-flex",
+        justifyContent: "center",
+        margin: "4rem 0 4rem 0"
+    }
+
     let getReply = async () => {
+        setLoading(true)
         try {
             let reply = await fetch("https://ainotesassistant.onrender.com/api/notes");
             let response = await reply.json();
             setData(response);
+            setLoading(false)
         } catch (e) {
             console.log(e)
         }
@@ -17,25 +29,33 @@ function HistoryPage() {
         getReply();
     }, [])
 
+    console.log(data)
     // getReply();
     return (
         <div className="d-flex flex-column pt-5 mb-5" style={{
             width: "1100px",
             maxWidth: "100%",
             margin: "0 auto",
+            padding: "0 2rem"
 
         }} >
             <h2 className="mb-5 text-muted">Search History</h2>
-            <div>
-                {data.length > 0 ? (
-                    data.map((data, index) => (
-                        <HistoryBox key={index} data={data} setData={setData} />
-                    ))
-                ) : (
-                    <p>No history found</p>
-                )}
+            <div style={{
+                minHeight: "24.5dvh",
+                display: "flex",
+                flexDirection: "column"
+            }}>{
+                    loading ? (<RiseLoader color="#ff6b01" loading={loading} cssOverride={override}></RiseLoader>) :
+                        (data.length > 0 ? (
+                            data.map((data, index) => (
+                                <HistoryBox key={index} data={data} setData={setData} />
+                            ))
+                        ) : (
+                            <p>No history found</p>
+                        ))
+                }
             </div>
-        </div>
+        </div >
     );
 }
 
